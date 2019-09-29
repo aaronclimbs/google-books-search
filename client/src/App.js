3953f8fd027747541567cc4f7ce7a4d60604f9b4
Aaron Keisler <amkeisler@gmail.com>
2019-09-29 14:36:54 -0400

    update client app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Navbar from "./components/AppNavbar";
+import BookList from "./components/BookList";
+import BookModal from "./components/AddModal";
+import { Container } from "reactstrap";
 
 function App() {
-  return <div></div>;
+  return (
+    <Provider store={store}>
+      <div>
+        <Navbar />
+        <Container>
+          <BookModal />
+        </Container>
+        <BookList />
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
