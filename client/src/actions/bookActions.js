be5d5204efbdf179595c1e60268fa221a4a10e56
Aaron Keisler <amkeisler@gmail.com>
2019-09-30 03:02:09 -0400

    refactor bookActions to include auth

--- client/src/actions/bookActions.js
+++ client/src/actions/bookActions.js
@@ -1,35 +1,45 @@
 import axios from "axios";
 import { ADD_BOOK, DELETE_BOOK, GET_BOOKS, BOOKS_LOADING } from "../actions/types";
+import { tokenConfig } from "./authActions";
+import { returnErrors } from "./errorActions";
 
 export const getBooks = () => dispatch => {
   dispatch(setLoading());
-  axios.get("/api/books").then(res =>
-    dispatch({
-      type: GET_BOOKS,
-      payload: res.data
-    })
-  );
-  // .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
+  axios
+    .get("/api/books")
+    .then(res =>
+      dispatch({
+        type: GET_BOOKS,
+        payload: res.data
+      })
+    )
+    .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };
 
-export const deleteBook = id => dispatch => {
+export const deleteBook = id => (dispatch, getState) => {
   dispatch(setLoading());
-  axios.delete(`/api/books/${id}`).then(res =>
-    dispatch({
-      type: DELETE_BOOK,
-      payload: res.data
-    })
-  );
+  axios
+    .delete(`/api/books/${id}`, tokenConfig(getState))
+    .then(res =>
+      dispatch({
+        type: DELETE_BOOK,
+        payload: res.data
+      })
+    )
+    .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };
 
-export const addBook = book => dispatch => {
+export const addBook = book => (dispatch, getState) => {
   dispatch(setLoading());
-  axios.post("/api/books", book).then(res =>
-    dispatch({
-      type: ADD_BOOK,
-      payload: res.data
-    })
-  );
+  axios
+    .post("/api/books", book, tokenConfig(getState))
+    .then(res =>
+      dispatch({
+        type: ADD_BOOK,
+        payload: res.data
+      })
+    )
+    .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };
 
 export const setLoading = () => {
