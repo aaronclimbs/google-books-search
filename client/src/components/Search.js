08af096f47e3e0fc0988b793e34c5fb8fc3b0a3f
Aaron Keisler <amkeisler@gmail.com>
2019-09-30 03:04:00 -0400

    base styling for search component

---
+++ client/src/components/Search.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class BookSearch extends Component {
+  state = {
+    search: "",
+    results: ""
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
