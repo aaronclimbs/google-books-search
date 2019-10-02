ff6f0e040effd3a0fd7b1a94465ce80986b83f40
Aaron Keisler <amkeisler@gmail.com>
2019-10-02 02:57:00 -0400

    add search route handling

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const bookRoutes = require("./books");
 const userRoutes = require("./users");
 const authRoutes = require("./auth");
+const searchRoutes = require("./search");
 
 // Book routes
 router.use("/books", bookRoutes);
 router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
+router.use("/search", searchRoutes);
 
 module.exports = router;
