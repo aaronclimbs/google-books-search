c3b1d0a0482203fa1b072f17b127b6e90f2f3563
Aaron Keisler <amkeisler@gmail.com>
2019-10-02 02:57:41 -0400

    add user  ook association handling

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,20 +2,10 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt");
 // require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const { User, Book } = require("../../models/");
+const auth = require("../../middleware/auth");
 
-// // Matches with "/api/user"
-// router
-//   .route("/")
-//   .get(userController.findAll)
-//   .post(userController.create);
-
-// // Matches with "/api/user/:id"
-// router
-//   .route("/:id")
-//   .get(userController.findById)
-//   .put(userController.update)
-//   .delete(userController.remove);
-
+// signup user
 router.post("/", (req, res) => {
   const { name, email, password } = req.body;
 
@@ -40,19 +30,71 @@ router.post("/", (req, res) => {
         if (err) throw err;
         newUser.password = hash;
         newUser.save().then(user => {
-          jwt.sign({ id: user.id }, process.env.jwtSecret, { expiresIn: 3600 }, (err, token) => {
-            if (err) throw err;
-            res.json({
-              token,
-              user: { id: user._id, name: user.name, email: user.email }
-            });
-          });
+          jwt.sign(
+            { id: user.id },
+            process.env.jwtSecret,
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) throw err;
+              res.json({
+                token,
+                user: { id: user._id, name: user.name, email: user.email }
+              });
+            }
+          );
         });
       });
     });
   });
 });
 
-const User = require("../../models/user");
+// add book to user
+router.post("/:userId", (req, res) => {
+  const newBook = req.body;
+  Book.create(newBook)
+    .then(data => {
+      const user = req.params.userId;
+      User.findByIdAndUpdate(
+        user,
+        { $push: { saved_books: data._id } },
+        { upsert: true, new: true }
+      )
+        .then(updatedUser => {
+          res.json(updatedUser);
+        })
+        .catch(err => {
+          res.json(err);
+        });
+    })
+    .catch(err => res.json(err));
+});
+
+// get books from user
+router.get("/:userId/books", (req, res) => {
+  User.findById(req.params.userId)
+    .populate("saved_books")
+    .exec((err, user) => {
+      if (err) res.json(err);
+      res.json({
+        books: user.saved_books
+      });
+    });
+});
+
+// delete book from user
+router.delete("/:userId", (req, res) => {
+  const user = req.params.userId;
+  User.findByIdAndUpdate(
+    user,
+    { $pull: { saved_books: req.body.bookId } },
+    { upsert: true, new: true }
+  )
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
